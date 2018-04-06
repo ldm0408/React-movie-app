8982d6b5e1161755262ddb7a37f131be6fa7b814
ldm0408 <ldm0408@gmail.com>
2018-04-06 18:19:22 +0900

    리액트 무비앱 4일차

--- src/App.js
+++ src/App.js
@@ -4,41 +4,31 @@ import Movie from './movie';
 
 class App extends Component {
 
-  state ={
+ state={
+
+ }
+  componentDidMount() {
+    this._getMovies()
   }
 
-  componentDidMount() {
-    setTimeout(()=>{
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster: "http://2korea.hani.co.kr/files/attach/images/64/981/316/001.jpg"
-          },
-          {
-            title: "Full Metal Jacket",
-            poster: "http://www.buddhismjournal.com/news/photo/201603/13910_18539_2211.jpg"
-          },
-          {
-            title: "Oldboy",
-            poster: "https://upload.wikimedia.org/wikipedia/ko/4/48/Old_Boy.jpg"
-          },
-          {
-            title: "Star Wars",
-            poster: "https://upload.wikimedia.org/wikipedia/ko/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg"
-          },
-          {
-            title: "Trainspotting",
-            poster: "http://th.soundsblog.it/da3Er_xYkiyZ2LVCWhkXqoc_VkU=/fit-in/655xorig/http%3A%2F%2Fmedia.soundsblog.it%2F4%2F4d2%2Ftrainspotting.png"
-          }
-        ]
-      })
-    },5000)
+  _getMovies = async ()=>{
+    const movies = await this._callApi()
+    this.setState({
+      movies : movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err))
   }
 
 _renderMovies = ()=> {
-  const movies = this.state.movies.map((movie, index) => {
-    return <Movie title={movie.title} poster={movie.poster} key={index} />
+  const movies = this.state.movies.map((movie) => {
+    return <Movie title={movie.title} poster={movie.large_cover_image
+} key={movie.id} />
   })
   return movies
 }
--- src/movie.js
+++ src/movie.js
@@ -1,34 +1,60 @@
-import React, { Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './movie.css';
 
-class Movie extends Component {
+// class Movie extends Component {
 
-    static propTypes ={
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
+//     static propTypes = {
+//         title: PropTypes.string.isRequired,
+//         poster: PropTypes.string.isRequired
+//     }
 
-    render(){
-        return(
-            <div>
-                <Movieposter poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+//     render() {
+//         return (
+//             <div>
+//                 <Movieposter poster={this.props.poster} />
+//                 <h1>{this.props.title}</h1>
+//             </div>
+//         )
+//     }
+// }
+
+function Movie ({title,poster}){
+    return(
+        <div>
+            <Movieposter poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    )
 }
-class Movieposter extends Component {
 
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    }
+// class Movieposter extends Component {
 
-    render() {
-        return (
-            <img src={this.props.poster} alt="movie poster" />
-        )
-    }
+//     static propTypes = {
+//         poster: PropTypes.string.isRequired
+//     }
+
+//     render() {
+//         return (
+//             <img src={this.props.poster} alt="movie poster" />
+//         )
+//     }
+// }
+
+
+// 클래스 컴퍼넌트를 functional 컴포넌트로 변환
+function Movieposter({poster}){
+    return(
+        <img src={poster} alt="movie poster" />
+    )
+}
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
+
+Movieposter.propTypes = {
+    poster: PropTypes.string.isRequired
 }
 
 
