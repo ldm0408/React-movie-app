6a6a47cad48430776457ebc07a06345d23177220
ldm0408 <ldm0408@gmail.com>
2018-04-10 16:33:11 +0900

    리액트 무비앱 5일차(완성)

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ class App extends Component {
   }
 
   _getMovies = async ()=>{
-    const movies = await this._callApi()
+    const movies = await this._callApi() //이 라인 작업이 완료 되기전엔 16행~19행이 실행 하지않는다
     this.setState({
-      movies : movies
+      movies //모던 자바스크립트 문법
     })
   }
 
@@ -27,16 +27,21 @@ class App extends Component {
 
 _renderMovies = ()=> {
   const movies = this.state.movies.map((movie) => {
-    return <Movie title={movie.title} poster={movie.large_cover_image
-} key={movie.id} />
+    return <Movie 
+      title={movie.title_english} 
+      poster={movie.medium_cover_image}
+      key={movie.id} 
+      genres={movie.genres}
+      synopsis={movie.synopsis} />
   })
   return movies
 }
 
   render() {
+    const {movies} = this.state;
     return (
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : 'Loading'}
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : 'Loading'}
  
       </div>
     );
--- src/movie.js
+++ src/movie.js
@@ -1,61 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './movie.css';
+import LinesEllipsis from 'react-lines-ellipsis'
 
-// class Movie extends Component {
-
-//     static propTypes = {
-//         title: PropTypes.string.isRequired,
-//         poster: PropTypes.string.isRequired
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 <Movieposter poster={this.props.poster} />
-//                 <h1>{this.props.title}</h1>
-//             </div>
-//         )
-//     }
-// }
-
-function Movie ({title,poster}){
+function Movie ({title,poster,genres,synopsis}){
     return(
-        <div>
-            <Movieposter poster={poster} />
-            <h1>{title}</h1>
+        <div className="Movie">
+            <div className="Movie__Column">
+            <Movieposter poster={poster} alt={title}/>
+            </div>
+            <div className="Movie__Column">
+             <h1>{title}</h1>
+             <div className="Movie__Genres">
+                {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+             </div>
+             <div className="Movie__Synopsis">
+                <LinesEllipsis
+                    text={synopsis}
+                    maxLine='3'
+                    ellipsis='...'
+                    trimRight
+                    basedOn='letters'
+                    />
+             </div>
+            </div>
         </div>
     )
 }
 
-// class Movieposter extends Component {
-
-//     static propTypes = {
-//         poster: PropTypes.string.isRequired
-//     }
-
-//     render() {
-//         return (
-//             <img src={this.props.poster} alt="movie poster" />
-//         )
-//     }
-// }
-
-
 // 클래스 컴퍼넌트를 functional 컴포넌트로 변환
-function Movieposter({poster}){
+function Movieposter({poster, alt}){
     return(
-        <img src={poster} alt="movie poster" />
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
     )
 }
+
+function MovieGenre({genre}){
+    return (
+        <span className="Movie__Genre">{genre}</span>
+    )
+}
+
 Movie.propTypes = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
 }
 
 Movieposter.propTypes = {
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
 }
-
+MovieGenre.propTypes = {
+    genre: PropTypes.string.isRequired
+}
 
 export default Movie;
